2e26415affc5e0bec38c23ad318689effcfec228
Aciem <43767240+Aciem22@users.noreply.github.com>
2025-11-03 12:56:54 -0300

    Update sheets.py
    ajuste sheets 4.0

--- utils/sheets.py
+++ utils/sheets.py
@@ -28,7 +28,7 @@ def get_creds():
                 "client_secret": client_config["client_secret"],
                 "auth_uri": "https://accounts.google.com/o/oauth2/auth",
                 "token_uri": "https://oauth2.googleapis.com/token",
-                "redirect_uris": ["https://<seu-usuario>.streamlitapp.io/"]
+                "redirect_uris": ["https://aciem22.streamlitapp.io/"]
             }
         },
         scopes=[
